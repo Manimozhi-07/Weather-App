8689e23351b02615295bc6b014b800561d498573
Manimozhi Neethimohan <Manimozhin@gain-insights.com>
2023-04-06 16:36:28 +0530

    changes

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ async function weather(city) {
     err.style.display = "block";
   } 
   else {
-    document.querySelector("#temp").innerText =Math.round(data.main.temp) + "°C";
+    document.querySelector("#temp").innerText =Math.round(data.main.temp) + "°c";
     document.querySelector("#state").innerText=data.weather[0].main;
-    document.querySelector("#city").innerText = data.name;
-    document.querySelector("#country").innerText = data.sys.country;
+    document.querySelector("#city").innerText = data.name + "," + data.sys.country;
+
     document.querySelector("#humidity").innerText = data.main.humidity + "%";
     document.querySelector("#wind").innerText = data.wind.speed + "km/hr";
     if (data.weather[0].main == "Clouds") {
@@ -34,17 +34,26 @@ async function weather(city) {
       image.src = "images/mist.png";
     } else if (data.weather[0].main == "Snow") {
       image.src = "images/snow.jpg";
+    }else if (data.weather[0].main == "Haze") {
+        image.src = "images/haze.png";
+    }
+    else if(data.weather[0].main=="Smoke"){
+      image.src="images/smoke.png";
     }
     weatherContainer.style.display = "block";
     err.style.display = "none";
   }
 }
 searchBtn.addEventListener("click", () => {
+    if(searchBox.value.trim().length=="0")
+        alert("Please enter city name");
   weather(searchBox.value);
   searchBox.value = "";
 });
 searchBox.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
+    if(searchBox.value.trim().length=="0")
+        alert("Please enter city name");
     weather(searchBox.value);
     searchBox.value = "";
   }
